28343ed2ed502b9c5dce46dc6c5f3e3bacaafadf
Prapangkorn Thangsathityangkul <XxtheNerdLordxX@outlook.com>
2026-01-29 01:01:12 +0700

    Refactor code structure for improved readability and maintainability

--- gateway/src/main/java/com/ecommerce/gateway/GatewayConfig.java
+++ gateway/src/main/java/com/ecommerce/gateway/GatewayConfig.java
@@ -1,4 +1,32 @@
 package com.ecommerce.gateway;
 
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class GatewayConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route("product-service", r -> r
+                        .path("/api/product/**")
+                        .uri("lb://PRODUCT-SERVICE"))
+                .route("user-service", r -> r
+                        .path("/api/users/**")
+                        .uri("lb://USER-SERVICE"))
+                .route("order-service", r -> r
+                        .path("/api/order/**", "/api/cart/**")
+                        .uri("lb://ORDER-SERVICE"))
+                .route("eureka-server", r -> r
+                        .path("/eureka/main")
+                        .filters(f -> f.setPath("/"))
+                        .uri("http://localhost:8761"))
+                .route("eureka-server-static", r -> r
+                        .path("/eureka/**")
+                        .uri("http://localhost:8761"))
+                .build();
+    }
 }
